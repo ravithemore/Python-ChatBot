501183657b1906068028b7003a3b91ae0e2a974c
Ravi M Damor <87522195+ravithemore@users.noreply.github.com>
2021-12-06 09:57:03 +0530

    Update pythonchatbot.py

--- pythonchatbot.py
+++ pythonchatbot.py
@@ -19,7 +19,7 @@ elif city.lower() == "tokyo":
 elif city.lower() == "los angeles":
     print ("Hey! I am in Hollywood, the land of glamour, palm trees, and sunny skies!")
 elif city.lower() == "India":
-    print ("Namaste! I am from India, the jungle of electric lights and colors at night!")  
+    print ("Namaste! I am from India, the land of!")  
 else:
     print ("You need to select from Paris, Tokyo, or Los Angeles. :)")
 
